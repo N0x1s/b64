2dd13a48730e566a4073c22a63a67cd9a4575e3b
n0x1s <ilyasselfatimy9@gmail.com>
2020-05-08 23:32:02 +0100

    replace encode and decode methods with convert

--- data_b64/core.py
+++ data_b64/core.py
@@ -3,27 +3,22 @@ class Media2B64:
     """ a class to convert online/offline media to base64"""
     __version__ = '0.1'
 
-    def __init__(self, source:str = None):
+    def __init__(self, source:str = str()):
         if source:
             self.source = source
             self.data = source.encode('utf-8')
 
-    def encode(self, data: bytes = None):
-        """ convert from bytes to b64 """
+    def convert_to(self, destination: str, data: bytes = None) -> bytes:
+        """ a method to handle conversation between base64 and bytes  """
+        f = base64.b64decode if destination == 'string' else base64.b64encode
         if data:
-            return base64.b64encode(data)
-        return base64.b64encode(self.data)
-
-    def decode(self, data: bytes = None):
-        """ convert b64 to bytes """
-        if data:
-            return base64.b64decode(data)
-        return base64.b64decode(self.data)
+            return f(data)
+        return f(self.data)
 
 """
 #### tests ####
 b64_strings = 'dGVzdA=='
-string = 'test'
-r = Media2B64(string).encode()
+string = b'test'
+r = Media2B64().convert_to('string', b64_strings)
 print(r)
 """
