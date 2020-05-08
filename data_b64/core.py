cd492438f2cba92f034e6dc934ed4758e8680c71
n0x1s <ilyasselfatimy9@gmail.com>
2020-05-08 23:05:55 +0100

    added encode decode methods

--- data_b64/core.py
+++ data_b64/core.py
@@ -1,3 +1,14 @@
+import base64
 class Media2B64:
     """ a class to convert online/offline media to base64"""
     __version__ = '0.1'
+    def __init__(self, data):
+        self.data = data
+
+    def encode(self, data: bytes = None):
+        """ convert from bytes to b64 """
+        return base64.b64encode(self.data)
+
+    def decode(self, data: bytes = None):
+        """ convert b64 to bytes """
+        return base6.b64decode(self.data)
